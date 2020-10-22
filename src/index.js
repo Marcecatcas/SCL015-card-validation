6db5f8b52b9678af5b7c855c94cc233c9884ae51
Saya Yaczms <marce.catalan.castelli@gmail.com>
2020-10-22 01:24:12 -0300

    actualizacion 4

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+//traer elemento de formulario y declarar la funcion onclick 
+ 
+function sendValue(){
+    const cardNumber = document.getElementsByClassName("validationbutton");
+    cardNumber = prompt("funciona");
+}
+
+ 
