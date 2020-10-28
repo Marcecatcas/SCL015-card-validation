aa1bbc2b6b1d9eeda1b820cec0cc6f85546e0a84
Saya Yaczms <marce.catalan.castelli@gmail.com>
2020-10-28 20:17:55 -0300

    actualizacion 8

--- src/index.js
+++ src/index.js
@@ -1,27 +1,19 @@
 import validator from './validator.js';
 
-console.log(validator);
+ //console.log(validator); 
 
-//traer elemento del formulario para validar la tarjeta
-const validate = document.getElementsByClassName("button");
+const validate = document.getElementById('submit');
 let creditCardNumber = '';
-//por medio del onsubmit traer el elemento de texto y enlazar a la funcion validadora.
-validate.onsubmit = function cardValidate() {
-    /* por medio de un bucle do obligamos a que se genere el documento, el cual se guardara en una variable en caso de que esta
-    cumpla con las condiciones dadas entro del ciclo.*/
-    do { //primero se obtiene la conexion entre el valor y la funcion isVal
-    creditCardNumber = document.getElementById("number").value;
-    if (creditCardNumber !== '') {
-        isValid(creditCardNumber);
-    } else {
-        alert("por favor ingrese un valor valido");
-    }
-    if (creditCardNumber > 4){
-        maskify(creditCardNumber);
-    }
-  }   
-  
-    while(creditCardNumber === '');
 
+validate.addEventListener('click',validar)
+function validar(){
+   creditCardNumber = document.getElementById('number').value;
+   if(creditCardNumber !='') {
+    isValid(creditCardNumber);
+   }
+  if(creditCardNumber > 4) {
+    maskify(creditCardNumber);
+} else{
+    alert("por favor ingresa numero valido");
 }
-                 
+}
--- src/validator.js
+++ src/validator.js
@@ -1,34 +1,33 @@
 const validator = {
-  function isValid(creditCardNumber){                         //primero armamos la funcion para validar por Algoritmo de luhn
-    let array = Array.from(creditCardNumber);                 //pedimos que genere un array desde el numero obtenido en el archivo index.js
-    inverseArray = array.reverse();                           //invertimos el array usado
-    for(let i = 0; i<=inverseArray.length; i++){              //aplicamos un ciclo for donde definimos variable, largo a evaluar y el aumento
-        if(i%2 === 0){                                        //por medio de if le decimos que tome todo lo que residuo de 2 de 0 (par)                                               
-        let evenNumber=parseInt(reversearr[i])*2;             //se pasa el valor obtenido de string a number por medio de parseInt y se multiplica por 2
-        if( evenNumber > 9 ) {                                //agregamos una condicion if... else adicional para separar los mayores y menores de 10
-          evenNumber = evenNumber -= 9;}                      //si el numero al ser multiplicado es mayor a 9 se le resta 9 al resultado  
-        else (evenNumber <= 9) {                              
-          evenNumber = evenNumber * 1;                        //si es menor o igual a 9 se multiplica por 1 para conservar el numero.
-        }
-        evensum += evenNumber;                                // pedimos la sumatoria de todos los numeros por medio del +=
-        }                                                     // cerramos el if... else anterior y abrimos otro para los numeros impares
-      else (i%2 !== 0){                                       // si el residuo es distinto a 0, aplicamos otra variable
-        let oddNumber=parseInt(reversearr[i]);                // le pedimos igual que antes que pase a number el string del array
-        oddsum += oddNumber;                                  // generamos la suma de estos valores
+  isValid:(creditCardNumber) => {
+    let evenSum;
+    let oddSum;
+    let sum = 0;
+    let array = Array.from(creditCardNumber);
+    let inverseArray = array.reverse();
+    for(let i=0; i <= inverseArray.length; i++){
+    if(i%2 == 0){
+      let evenNumber=parseInt(inverseArray)*2;
+      if(evenNumber > 9) {
+        evenNumber = evenNumber -= 9;
       }
-      totalsum = pairsum + oddsum;                            // pedimos la suma total del array 
-    }
-      if(totalsum % 10 === 0){                                //para terminar de validar, si el residuo es 0. la tarjeta es valida.
-      document.write("tu tarjeta es valida, puedes usarla en el comercio.");
-    }
-      else(totalsum % 10 !== 0){                              // si el residuo es distinto de 0 la tarjeta es invalida.
-      document.write("tu tarjeta es invalida, por favor comunicate a la brevedad al 8007000100 para ayudarte.")
+      else{
+        evenNumber = evenNumber * 1;
       }
+      evenSum += evenNumber;
     }
-  function Maskify(creditCardNumber);
-
+    else {
+      let oddNumber = parseInt(inverseArray);
+      oddSum += oddNumber;
+    }
+      sum = evenSum + oddSum;
+    }
+    if(sum % 10 === 0){
+      alert("tu tarjeta es valida, puedes usarla en el comercio.");
+    } else {
+      alert("tu tarjeta es invalida, por favor comunicate a la brevedad al 8007000100.")
+    }
   }
-
-
-export default validator;
+}
+export {validator}
 // aqui van las funciones Valid y Maskify
